d61ef7b72456fb1a0418626ef12696970c071587
Agustín Miranda <agustinm.a@protonmail.com>
2026-02-16 23:39:33 -0300

    Adding Metabase support

--- src/main/java/com/app/SpringSecurityAppApplication.java
+++ src/main/java/com/app/SpringSecurityAppApplication.java
@@ -22,93 +22,93 @@ public class SpringSecurityAppApplication {
         SpringApplication.run(SpringSecurityAppApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            /* Crear PERMISOS */
-            PermissionEntity createPermission = PermissionEntity.builder()
-                    .name("CREATE")
-                    .build();
-
-            PermissionEntity readPermission = PermissionEntity.builder()
-                    .name("READ")
-                    .build();
-
-            PermissionEntity updatePermission = PermissionEntity.builder()
-                    .name("UPDATE")
-                    .build();
-
-            PermissionEntity deletePermission = PermissionEntity.builder()
-                    .name("DELETE")
-                    .build();
-
-            PermissionEntity refactorPermission = PermissionEntity.builder()
-                    .name("REFACTOR")
-                    .build();
-
-            /* Crear ROLEA */
-            RoleEntity roleAdmin = RoleEntity.builder()
-                    .roleEnum(RoleEnum.ADMIN)
-                    .permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission))
-                    .build();
-
-            RoleEntity roleUser = RoleEntity.builder()
-                    .roleEnum(RoleEnum.USER)
-                    .permissionList(Set.of(createPermission, readPermission))
-                    .build();
-
-            RoleEntity roleInvited = RoleEntity.builder()
-                    .roleEnum(RoleEnum.INVITED)
-                    .permissionList(Set.of(readPermission))
-                    .build();
-
-            RoleEntity roleDeveloper = RoleEntity.builder()
-                    .roleEnum(RoleEnum.DEVELOPER)
-                    .permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
-                    .build();
-
-            /* Crear USUARIOS */
-            UserEntity userSantiago = UserEntity.builder()
-                    .username("santiago")
-                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialNoExpired(true)
-                    .roles(Set.of(roleAdmin))
-                    .build();
-
-            UserEntity userDaniel = UserEntity.builder()
-                    .username("daniel")
-                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialNoExpired(true)
-                    .roles(Set.of(roleUser))
-                    .build();
-
-            UserEntity userAndrea = UserEntity.builder()
-                    .username("andrea")
-                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialNoExpired(true)
-                    .roles(Set.of(roleInvited))
-                    .build();
-
-            UserEntity userAnyi = UserEntity.builder()
-                    .username("anyi")
-                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
-                    .isEnabled(true)
-                    .accountNoExpired(true)
-                    .accountNoLocked(true)
-                    .credentialNoExpired(true)
-                    .roles(Set.of(roleDeveloper))
-                    .build();
-
-            userRepository.saveAll(List.of(userSantiago, userDaniel, userAndrea, userAnyi));
-        };
-    }
+//    @Bean
+//    CommandLineRunner init(UserRepository userRepository) {
+//        return args -> {
+//            /* Crear PERMISOS */
+//            PermissionEntity createPermission = PermissionEntity.builder()
+//                    .name("CREATE")
+//                    .build();
+//
+//            PermissionEntity readPermission = PermissionEntity.builder()
+//                    .name("READ")
+//                    .build();
+//
+//            PermissionEntity updatePermission = PermissionEntity.builder()
+//                    .name("UPDATE")
+//                    .build();
+//
+//            PermissionEntity deletePermission = PermissionEntity.builder()
+//                    .name("DELETE")
+//                    .build();
+//
+//            PermissionEntity refactorPermission = PermissionEntity.builder()
+//                    .name("REFACTOR")
+//                    .build();
+//
+//            /* Crear ROLEA */
+//            RoleEntity roleAdmin = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.ADMIN)
+//                    .permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission))
+//                    .build();
+//
+//            RoleEntity roleUser = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.USER)
+//                    .permissionList(Set.of(createPermission, readPermission))
+//                    .build();
+//
+//            RoleEntity roleInvited = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.INVITED)
+//                    .permissionList(Set.of(readPermission))
+//                    .build();
+//
+//            RoleEntity roleDeveloper = RoleEntity.builder()
+//                    .roleEnum(RoleEnum.DEVELOPER)
+//                    .permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
+//                    .build();
+//
+//            /* Crear USUARIOS */
+//            UserEntity userSantiago = UserEntity.builder()
+//                    .username("santiago")
+//                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialNoExpired(true)
+//                    .roles(Set.of(roleAdmin))
+//                    .build();
+//
+//            UserEntity userDaniel = UserEntity.builder()
+//                    .username("daniel")
+//                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialNoExpired(true)
+//                    .roles(Set.of(roleUser))
+//                    .build();
+//
+//            UserEntity userAndrea = UserEntity.builder()
+//                    .username("andrea")
+//                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialNoExpired(true)
+//                    .roles(Set.of(roleInvited))
+//                    .build();
+//
+//            UserEntity userAnyi = UserEntity.builder()
+//                    .username("anyi")
+//                    .password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+//                    .isEnabled(true)
+//                    .accountNoExpired(true)
+//                    .accountNoLocked(true)
+//                    .credentialNoExpired(true)
+//                    .roles(Set.of(roleDeveloper))
+//                    .build();
+//
+//            userRepository.saveAll(List.of(userSantiago, userDaniel, userAndrea, userAnyi));
+//        };
+//    }
 }
